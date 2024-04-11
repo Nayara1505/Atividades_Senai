18b3dba9f437025f8026d08fea657aef478a5c43
Nayara1505 <165666440+Nayara1505@users.noreply.github.com>
2024-04-11 18:34:56 -0300

    Create Questão01.py

---
+++ Classes e objetos em python 2/Questão01.py
@@ -0,0 +1,15 @@
+#Crie uma classe que modele uma pessoa 
+(a) Atributos: nome, idade e endereço 
+(b) Metodos: mostrar endereço e alterar endereço
+
+class Pessoa:
+    def __init__(self, nome, idade, endereco):
+        self.nome = nome
+        self.idade = idade
+        self.endereco = endereco
+
+    def mostrar_endereco(self):
+        return self.endereco
+
+    def alterar_endereco(self, novo_endereco):
+        self.endereco = novo_endereco
