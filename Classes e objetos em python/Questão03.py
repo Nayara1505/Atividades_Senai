34727644850a329f76b1166f70a6909613472c86
Nayara1505 <165666440+Nayara1505@users.noreply.github.com>
2024-04-11 18:28:08 -0300

    Create Questão03.py

---
+++ Classes e objetos em python/Questão03.py
@@ -0,0 +1,24 @@
+#Crie uma classe que modele uma pessoa:
+Atributos: nome, idade, peso e altura
+Métodos: Envelhercer, engordar, emagrecer, crescer. Obs: Por padrão, a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.
+
+class Pessoa:
+    def __init__(self, nome, idade, peso, altura):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura
+
+    def envelhecer(self, anos):
+        self.idade += anos
+        if self.idade < 21:
+            self.crescer(0.5 * anos)
+
+    def engordar(self, peso):
+        self.peso += peso
+
+    def emagrecer(self, peso):
+        self.peso -= peso
+
+    def crescer(self, altura):
+        self.altura += altura
