971120657ec18553829c3462ed501494dfa86bc2
Nayara1505 <165666440+Nayara1505@users.noreply.github.com>
2024-04-11 08:47:46 -0300

    Create Questão5.py

---
+++ Estrutura sequencial/Questão5.py
@@ -0,0 +1,4 @@
+#Faça um Programa que converta metros para centímetros.
+metros = float (input ("Digite um número: "))
+centimetros = metros*100
+print (f"{metros} metros em centimetros equivale a {centimetros}")
