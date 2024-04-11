140fa4d4926fe0558cddc712d72df9caed811bb1
Nayara1505 <165666440+Nayara1505@users.noreply.github.com>
2024-04-11 08:48:54 -0300

    Create Questão6.py

---
+++ Estrutura sequencial/Questão6.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+import math
+
+raio = float (input ("Digite o raio do circulo: "))
+area = math.pi * (raio ** 2)
+print (f"A area do circulo é {area}")
