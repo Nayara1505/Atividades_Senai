816955e3171966aef410e668f92154764a81cccd
Nayara1505 <165666440+Nayara1505@users.noreply.github.com>
2024-04-11 18:28:58 -0300

    Create Questão04.py

---
+++ Classes e objetos em python/Questão04.py
@@ -0,0 +1,24 @@
+#Crie uma classe para implementar uma conta corrente.
+A classe deve possuir os seguintes atributos: número da conta, nome do correntista e saldo.
+Os métodos são os seguintes: alterarNome, depósito e saque;
+No construtor, saldo é opcional, com valor default zero e os demais atributos são obrigatórios.
+
+class ContaCorrente:
+    def __init__(self, numero_conta, nome_correntista, saldo=0):
+        self.numero_conta = numero_conta
+        self.nome_correntista = nome_correntista
+        self.saldo = saldo
+
+    def alterar_nome(self, novo_nome):
+        self.nome_correntista = novo_nome
+
+    def deposito(self, valor):
+        self.saldo += valor
+
+    def saque(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            return True
+        else:
+            print("Saldo insuficiente.")
+            return False
