e0e31281fe971197d152032b30070bb619ec33a3
Nayara1505 <165666440+Nayara1505@users.noreply.github.com>
2024-04-11 08:50:25 -0300

    Create Questão7.py

---
+++ Estrutura sequencial/Questão7.py
@@ -0,0 +1,4 @@
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+lado = float (input ("Digite o lado do quadrado: "))
+area = lado ** 2
+print (f"A area do quadrado é {area} \nO dobro da area é: {area*2}")
