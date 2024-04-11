da7b0c751f57c33c5e8dafdf0ee8c1c3dbf4238e
Nayara1505 <165666440+Nayara1505@users.noreply.github.com>
2024-04-11 09:07:30 -0300

    Create Questão2.py

---
+++ Estruturas de comparação/Questão2.py
@@ -0,0 +1,9 @@
+#Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+
+letra = input("Digite uma letra (F ou M): ").upper()
+if letra == "F":
+    print("F - Feminino")
+elif letra == "M":
+    print("M - Masculino")
+else:
+    print("Sexo Inválido")
