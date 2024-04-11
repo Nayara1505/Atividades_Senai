8e27887105280445e66b57a821da318056238818
Nayara1505 <165666440+Nayara1505@users.noreply.github.com>
2024-04-11 08:51:01 -0300

    Create Questão8.py

---
+++ Estrutura sequencial/Questão8.py
@@ -0,0 +1,5 @@
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+valor_Por_Hora = float (input ("Digite o valor que você ganha por hora: "))
+num_Horas_Trabalho = float (input ("Digite o número de horas de trabalho no mês: "))
+salario = valor_Por_Hora * num_Horas_Trabalho
+print (f"O total do seu salário é {salario}")
