dffe1ad56fa4874bed4e890973e5a0ad8aa02166
Nayara1505 <165666440+Nayara1505@users.noreply.github.com>
2024-04-11 08:46:05 -0300

    Create Questão3.py

---
+++ Estrutura sequencial/Questão3.py
@@ -0,0 +1,5 @@
+#Faça um Programa que peça dois números e imprima a soma.
+num1 = float (input ("Digite o primeiro número: "))
+num2 = float (input ("Digite o segundo número: "))
+soma = num1 + num2
+print (f"A soma dos dois números é: {soma}")
