5d3e5489596c230fe7012eab5bda22d88a8b4a74
nayara <165666440+Nayara1505@users.noreply.github.com>
2024-04-13 19:46:53 -0300

    Create Q07.py

---
+++ Classes e objetos em python/Q07.py
@@ -0,0 +1,15 @@
+# Crie uma classe que modele uma aluno 
+(a) Atributos: nome, numero de matrıcula e curso 
+(b) Metodos: mostrar curso e alterar curso ´ 
+
+class Aluno:
+    def __init__(self, nome, numero_matricula, curso):
+        self.nome = nome
+        self.numero_matricula = numero_matricula
+        self.curso = curso
+
+    def mostrar_curso(self):
+        return self.curso
+
+    def alterar_curso(self, novo_curso):
+        self.curso = novo_curso
