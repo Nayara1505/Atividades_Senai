57ada0a715eb97bdf3d64f8de84521c6ff4e4c7e
nayara <165666440+Nayara1505@users.noreply.github.com>
2024-04-13 19:49:31 -0300

    Create Q09.py

---
+++ Classes e objetos em python/Q09.py
@@ -0,0 +1,36 @@
+#Crie uma classe para representar um horario (hora, minuto e segundo). Implemente os metodos para fazer as operacoes de incremento (de segundos) no horario e diferença ´entre dois horarios. 
+
+class Horario:
+    def __init__(self, hora, minuto, segundo):
+        self.hora = hora
+        self.minuto = minuto
+        self.segundo = segundo
+
+    def incrementar_segundo(self):
+        self.segundo += 1
+        if self.segundo == 60:
+            self.segundo = 0
+            self.minuto += 1
+            if self.minuto == 60:
+                self.minuto = 0
+                self.hora += 1
+                if self.hora == 24:
+                    self.hora = 0
+
+    def diferenca_horarios(self, outro_horario):
+        segundos_atual = self.hora * 3600 + self.minuto * 60 + self.segundo
+        segundos_outro = outro_horario.hora * 3600 + outro_horario.minuto * 60 + outro_horario.segundo
+        diferenca = abs(segundos_atual - segundos_outro)
+        return f"A diferença entre os horários é de {diferenca // 3600} horas, {(diferenca % 3600) // 60} minutos e {diferenca % 60} segundos."
+
+class Carro:
+    def __init__(self, marca, ano, preco):
+        self.marca = marca
+        self.ano = ano
+        self.preco = preco
+
+    def mostrar_preco(self):
+        return self.preco
+
+    def exibir_dados(self):
+        return f"Marca: {self.marca}, Ano: {self.ano}, Preço: R${self.preco:.2f}"
